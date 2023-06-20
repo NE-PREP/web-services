2fc33fb9eeb90143bb64566650e5c97f1c59f645
Nkubito Pacis <pacisnkubito@gmail.com>
2023-06-20 10:55:19 +0200

    Final commit

--- 2023-practical-exam/server/src/validators/user.validator.js
+++ 2023-practical-exam/server/src/validators/user.validator.js
@@ -4,6 +4,7 @@ import { errorResponse } from "../utils/api.response.js";
 export async function validateAdminRegistration(req, res, next) {
   try {
     const schema = Joi.object({
+      names: Joi.string().label("Names"),
       email: Joi.string().email().required().label("Email"),
       password: Joi.string().min(6).required().label("Password"),
     });
